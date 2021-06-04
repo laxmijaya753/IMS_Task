7dfd71918f3d13b3ffe17fde25e41fc7b2ab52da
laxmijaya753 <you@example.com>
2021-06-04 15:15:44 -0400

    Jaya:Changed files

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,8 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('?', '?');
+
+
+INSERT INTO  `ims`.`item` (item_id,name,value1) VALUES ('?','?','?');
+
+INSERT INTO  `ims`.`order_items` (customer_id,item_id1,quantity) VALUES ('?','?','?');
+
+INSERT INTO  `ims`.`orders` (customer_id1) VALUES ('?');
